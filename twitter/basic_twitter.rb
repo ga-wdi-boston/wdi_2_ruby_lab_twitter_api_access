74d2c47b4586b0ab48e778b6324317e29010228f
Abby Howell <abby.g.howell@gmail.com>
2013-09-30 17:15:35 -0400

    ongoing twitter work

--- twitter/basic_twitter.rb
+++ twitter/basic_twitter.rb
@@ -12,4 +12,95 @@ client = Twitter::REST::Client.new do |config|
   config.access_token_secret = ENV["ACCESS_SECRET"]
 end
 
-binding.pry
+
+# binding.pry
+
+def return_friends(client)
+	friends = []
+	client.friends.attrs[:users].each do |user|
+		friends << user[:name]
+	end
+	return friends
+end
+
+def return_favourites(client)
+	favourites = []
+	client.favourites.each do |tweet|
+		favourites << "#{tweet.attrs[:user][:screen_name]}: #{tweet.attrs[:text]}"
+	end
+	return favourites
+end
+
+puts "Welcome to @abbygezunt's Twitter page.  What would you like to see?"
+puts "[user] details, [recent] tweets, [friends] list, [lists], [fav]orited tweets"
+answer = gets.chomp
+
+case answer 
+when 'user'
+when 'recent'
+when 'friends'
+	puts "Abby's friends list:"
+	puts return_friends(client)
+when 'lists'
+when 'fav'
+	puts "Abby's favorites:"
+	puts return_favourites(client)
+else
+	puts "#{client.attrs[:name]} has #{client.attrs[:friends_count]} friends.  Their most recent status was: \n\"#{client.attrs[:status][:text]}\"."
+end 
+	
+
+
+# Recent Tweets, Friend List, User Details, Lists, Favorited Tweets
+
+# puts "Would you like to know about anyone else?  Type their username."
+# answer = gets.chomp
+# username = client.user(answer)
+
+# puts "#{username.attrs[:name]} has #{username.attrs[:friends_count]} friends.  Their most recent status was: \n\"#{username.attrs[:status][:text]}\"."
+
+
+# client.lists
+# client.favorites(user)
+# client.user.
+
+# client.sample do |tweet|
+#   puts tweet.text
+# end
+
+
+
+
+
+
+
+# client.user(username) do |tweet|
+#   puts tweet.text
+# end
+
+# client.friends.all
+
+# Twitter::User
+
+# friend list:
+# def friends_list(username)
+# 	friends = []
+# 	client.friends[:users].each do |friend|
+# 		friends << friend[:name]
+# 	end
+# 	return friends
+# end
+
+# puts friends_list(answer)
+
+
+
+# recent tweets: 
+# puts client.user_timeline(answer)
+
+
+# What would you like to see? 
+# Recent Tweets, Friend List, User Details, Lists, Favorited Tweets
+
+# client.update('Tweeting from Ruby #LearnedAtGA')
+# 
