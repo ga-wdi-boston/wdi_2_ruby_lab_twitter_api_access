0d2ff19888229ecae3676451ff6a6d1fb0463530
Bryce Lambert <brycelambert@gmail.com>
2013-09-30 23:29:50 -0400

    finished, but missing some desired working methods

--- app.rb
+++ app.rb
@@ -29,13 +29,14 @@ def list_followers(handle, client)
   return client.followers(handle).take(25).collect { |follower| follower.screen_name }
 end
 
-def list_mentions(handle, client)
-  return client.mentions(handle).collect { |mentions| mentions.text }
-end
+# def list_mentions(handle, client)
+#   return client.user(handle).mentions.collect { |mentions| mentions.text }
+# end
 
-def list_retweets(handle, client)
-  return client.retweeted_by_user(handle).take(25).collect { |tweets| tweets.screen_name }
-end
+# def list_retweets(handle, client)
+#   return client.retweeted_by_user(handle).take(25).collect 
+#   { |tweets| tweets.screen_name }
+# end
 
 def following_count(handle, client)
   count = 0
@@ -43,11 +44,11 @@ def following_count(handle, client)
   return count
 end
 
-def retweet_count(handle, client)
-  count = 0
-  client.retweeted_by_user(handle).each { |retweets| count += 1 }
-  return count
-end
+# def retweet_count(handle, client)
+#   count = 0
+#   client.retweeted_by_user(handle).each { |retweets| count += 1 }
+#   return count
+# end
 
 def tagged_ga(client)
  return client.search('#LearnedAtGA').collect { |tweet|
@@ -55,28 +56,26 @@ def tagged_ga(client)
 end
 
 def twitter_stats(handle, client)
-  follower_count = following_count(handle, client).to_f
+  follower_count = client.user(handle).followers_count.to_f
   follows_count = following_count(handle, client).to_f
   
-  puts "Friend Count: #{client.user(handle).friends_count}
+  return "Friend Count: #{client.user(handle).friends_count}
   Follows Count: #{follows_count}
   Follower Count: #{follower_count}
   Tweet Count: #{client.user(handle).tweets_count}
-  Retweets: #{retweet_count(handle, client)}
   Follows/Followers Ratio: #{follower_count / follows_count} "
 end
 
-handle = "bostonlowbrow"
+puts "The Twitter name:"
+handle = gets.chomp
 
 puts <<PARAGRAPH
 You can:
 1. list recent tweets
 2. list recent followers
-3. lsit mentions
-4. list retweets
-5. list basic stats
-6. list tweets tagged #LearnedAtGA
-7. list justin bieber mentions
+3. list stats
+4. list tweets tagged #LearnedAtGA
+5. list justin bieber mentions
 PARAGRAPH
 
 select = gets.chomp.to_i
@@ -87,18 +86,11 @@ case select
   when 2
     puts list_followers(handle, client)
   when 3
-    puts list_mentions(handle, client)
-  when 4
-    puts list_retweets(handle, client)
-  when 5
     puts twitter_stats(handle, client)
-  when 6
+  when 4
     puts tagged_ga(client)
-  when 7
+  when 5
     puts live_stream(client)
   end
 
-
-#FIX COUNTS IN 5
-#FIX mentions, retweets?
 #export SSL_CERT_FILE=/usr/local/opt/curl-ca-bundle/share/ca-bundle.crt
