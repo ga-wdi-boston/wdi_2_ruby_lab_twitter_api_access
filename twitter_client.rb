266c8bc88589b220f6279c95539cdfdfe85a9c93
Emma Spencer <emma.n.spencer@gmail.com>
2013-10-01 08:50:24 -0400

    Worked on getting methods to work

--- twitter_client.rb
+++ twitter_client.rb
@@ -16,7 +16,7 @@ username = gets.chomp
 # client.user(username)
 binding.pry
 puts "What would you like to view? Enter a number:"
-puts "1 - Most Recent Tweet"
+puts "1 - Recent Tweets"
 puts "2 - Friend Count"
 puts "3 - User Details"
 puts "4 - # of Lists Subscribed To"
@@ -27,44 +27,55 @@ input = gets.chomp
 
 case input
 when "1"
-	x = client.user(username).statuses.take(20)
-	#statuses doesn't work
+	recent_tweets = []
+ 		@client.user_timeline.each do |tweet|
+		recent_tweets << tweet.text
+			end  
+		end
 
 when "2"
 	# friends = []
 
 	friends_array = []
-  	client.friends(username.to_s).take(5).each do |friend|
+  	client.friends(username).take(5).each do |friend|
   		friends_array << friend.name
   		end
   	friends_array.each do |friend| puts friend 
   		end
+  		x = friends_array
 	# client.friends(username).take(20).each do |friend|
 	# 	friends << friend[:name]
 	# 	x = friends
 
 when "3"
 	x = client.user(username).description
-	#this works
 
 when "4"
-	x = client.lists(username)
-	#this works
+	client.lists(username) do |list|
+		x = list.text
+	end
 
 when "5"
 	# x = client.favorites(username).take(5)
 	favorites = []
 	client.favorites(username).take(5).each do |x|
 		favorites << x[:name] 
-		favorites
+		x = favorites
 		end
 
-	# favorites.each do |x| x
 when '6'
-	x = client.user(username).friends_count
+	friends_list = []
+	@client.friends(username) do |friends|
+		friends_list << friends.text
+		x = friends_list
+	end
 
 when "7"
-	x = client.mentions
+	mentions = []
+	client.mentions(username) do |mention|
+		mentions << mention.text
+	x = mentions
+	end
 
 else
 	puts "Try again."
