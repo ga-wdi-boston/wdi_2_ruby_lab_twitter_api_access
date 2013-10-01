f5ee86ded57f642c56c030267739fd02c21a5f78
Blake Ruddock <ruddock.blake@gmail.com>
2013-09-30 21:41:56 -0400

    Fixed error. Final commit

---
+++ twitter_client.rb
@@ -0,0 +1,16 @@
+require 'twitter'
+require 'pry'
+require 'dotenv'
+Dotenv.load
+
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV["CONSUMER_KEY"]
+  config.consumer_secret    = ENV["CONSUMER_SECRET"]
+  config.access_token        = ENV["ACCESS_TOKEN"]
+  config.access_token_secret = ENV["ACCESS_SECRET"]
+end
+
+binding.pry
